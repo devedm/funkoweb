301337864fac6ae4c564d60f543752c303cf76e9
Eddy Mena <developer.eddymena@gmail.com>
2024-12-04 17:30:04 -0600

    line cration threejs

--- src/index.js
+++ src/index.js
@@ -4,32 +4,30 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as THREE from 'three';
-
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+import { ThreeMFLoader } from 'three/examples/jsm/Addons.js';
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
+camera.position.set( 0 , 0 , 100 );
+camera.lookAt ( 0 , 0 , 0 );
 
-camera.position.z = 5;
+const scene = new THREE.Scene();
 
-function animate() {
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+//create a blue LineBasicMaterial
+const material = new THREE.LineBasicMaterial( { color: 'rgb(199,139,6)' } );
 
-	renderer.render( scene, camera );
-}
+const points = [];
+points.push( new THREE.Vector3( -10 , 0 , 0 ));
+points.push( new THREE.Vector3( 0 , 10 , 0 ));
+points.push( new THREE.Vector3( 10 , 0 , 0 ));
 
+const geometry = new THREE.BufferGeometry().setFromPoints( points );
 
+const line = new THREE.Line( geometry , material );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+scene.add( line );
+renderer.render( scene , camera );
+
